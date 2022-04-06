652a2fd34395fc4e0779d43fc423e4c543f9eba9
Ankit <negiankit705@gmail.com>
2022-04-06 23:46:17 +0530

    first commit

---
+++ foodOderingSystem/src/main/java/com/project/foodOderingSystem/FoodOderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.foodOderingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOderingSystemApplication.class, args);
+	}
+
+}
---
+++ foodOderingSystem/src/test/java/com/project/foodOderingSystem/FoodOderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.foodOderingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
