dfef3a86a5a79abf41a98067d704613a54dfb709
sanju212177 <84228994+sanju212177@users.noreply.github.com>
2022-04-08 16:30:06 +0530

    Add files via upload

---
+++ foodOderingSystem/src/main/java/com/project/foodOderingSystem/exception/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.project.foodOderingSystem.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+    public CustomerNotFoundException(String message){
+        super(message);
+    }
+}
